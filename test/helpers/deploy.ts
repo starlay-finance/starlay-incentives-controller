fff6e1506779230f6ab6a13e66c5db245d0f20e3
linnefromice <linnefromice@gmail.com>
2022-02-10 17:09:14 +0900

    modify deploy scripts

--- test/helpers/deploy.ts
+++ test/helpers/deploy.ts
@@ -36,7 +36,6 @@ export const testDeployIncentivesController = async (
 
   const incentivesImplementation = await deployStakedTokenIncentivesController([
     stakeProxy.address,
-    emissionManagerAddress,
   ]);
 
   // Initialize proxies
@@ -45,7 +44,7 @@ export const testDeployIncentivesController = async (
     'initialize()',
     []
   );
-  const incentivesInit = incentivesImplementation.interface.encodeFunctionData('initialize');
+  const incentivesInit = incentivesImplementation.interface.encodeFunctionData('initialize', [emissionManagerAddress]);
 
   await (
     await stakeProxy['initialize(address,address,bytes)'](
