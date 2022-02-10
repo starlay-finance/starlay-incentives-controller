fd2689dcf76820eda21239ac3b48ba31d8cd11eb
linnefromice <linnefromice@gmail.com>
2022-02-10 22:21:04 +0900

    add PullRewardsIncentivesController spec - check setted emissionManager by initializer

--- test/PullRewardsIncentivesController/misc.spec.ts
+++ test/PullRewardsIncentivesController/misc.spec.ts
@@ -3,10 +3,11 @@ import { timeLatest, waitForTx } from '../../helpers/misc-utils';
 import { expect } from 'chai';
 
 import { makeSuite } from '../helpers/make-suite';
-import { deployPullRewardsIncentivesController } from '../../helpers/contracts-accessors';
+import { deployInitializableAdminUpgradeabilityProxy, deployPullRewardsIncentivesController } from '../../helpers/contracts-accessors';
 import { MAX_UINT_AMOUNT, RANDOM_ADDRESSES, ZERO_ADDRESS } from '../../helpers/constants';
+import { PullRewardsIncentivesController__factory } from '../../types';
 
-makeSuite('pullRewardsIncentivesController misc tests', (testEnv) => {
+makeSuite('PullRewardsIncentivesController misc tests', (testEnv) => {
   it('constructor should assign correct params', async () => {
     const fakeToken = RANDOM_ADDRESSES[5];
 
@@ -17,6 +18,39 @@ makeSuite('pullRewardsIncentivesController misc tests', (testEnv) => {
     await expect((await pullRewardsIncentivesController.EMISSION_MANAGER()).toString()).to.be.equal(
       ZERO_ADDRESS
     );
+  });
+
+  it('initializer should assign correct params', async () => {
+    const { users } = testEnv;
+    const emissionManager = users[0];
+    const fakeToken = RANDOM_ADDRESSES[0];
+    const fakeRewardsVault = RANDOM_ADDRESSES[1];
+    const proxyAdmin = RANDOM_ADDRESSES[2];
+
+    const proxy = await deployInitializableAdminUpgradeabilityProxy();
+    const impl = await deployPullRewardsIncentivesController([
+      fakeToken
+    ]);
+    const encodedParams = impl.interface.encodeFunctionData('initialize', [
+      fakeRewardsVault,
+      emissionManager.address,
+    ]);
+    await (
+      await proxy.functions['initialize(address,address,bytes)'](
+        impl.address,
+        proxyAdmin,
+        encodedParams
+      )
+    ).wait();
+    const connectedImpl = PullRewardsIncentivesController__factory.connect(
+      proxy.address,
+      emissionManager.signer
+    );
+
+    await expect((await connectedImpl.getRewardsVault()).toString())
+      .to.be.equal(fakeRewardsVault);
+    await expect((await connectedImpl.EMISSION_MANAGER()).toString())
+      .to.be.equal(emissionManager.address);
   });
 
   it('Should return same index while multiple asset index updates', async () => {
