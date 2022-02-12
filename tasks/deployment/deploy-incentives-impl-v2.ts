933b165876aeb299f9e3a6dbd88c40f608b9d218
linnefromice <linnefromice@gmail.com>
2022-02-12 17:06:16 +0900

    update StakedToken address for shibuya

--- tasks/deployment/deploy-incentives-impl-v2.ts
+++ tasks/deployment/deploy-incentives-impl-v2.ts
@@ -6,7 +6,7 @@ import { waitForTx } from '../../helpers/misc-utils';
 import { eContractid } from '../../helpers/types';
 
 // Shibuya (astar network) addresses
-const STAKED_STARLAY = '0x5d666338118763ca0cF6719F479491B76bc88131'; // StakedLay's (StakedTokenV2Rev3) proxy
+const STAKED_STARLAY = '0x3bA5A4b88331627236378000B50eAa186375c3FF'; // StakedLay's (StakedTokenV2Rev3) proxy
 const STARLAY_SHORT_EXECUTOR = '0x6543076E4315bd82129105890Bc49c18f496a528'; // PoolAdmin
 
 task('deploy-incentives-impl-v2', 'Incentives controller implementation deployment').setAction(
