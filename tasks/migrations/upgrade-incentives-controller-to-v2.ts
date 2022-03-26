4bd2ea0bd62d1c7324fab1dcee517477bf5ec68e
atatur9 <momodarou2015@gmail.com>
2022-03-26 13:24:30 +0900

    fix todo

--- tasks/migrations/upgrade-incentives-controller-to-v2.ts
+++ tasks/migrations/upgrade-incentives-controller-to-v2.ts
@@ -12,13 +12,14 @@ import { isAddress } from 'ethers/lib/utils';
 import { Wallet } from 'ethers';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { deployPullRewardsIncentivesControllerV2 } from '../../helpers/contracts-accessors';
+import { getParamPerNetwork } from '../../helpers/contracts-helpers';
+import { NETWORKS_RPC_URL } from '../../helper-hardhat-config';
 require('dotenv').config();
 
 task(
   'upgrade-incentives-controller-to-v2',
   'Upgrade PullRewardsIncentivesController to V2'
 ).setAction(async ({}, localBRE) => {
-  // TODO: rpc url by environments
   await localBRE.run('set-DRE');
   const networkName = localBRE.network.name as eNetwork;
   const EMISSION_MANAGER_PRIVATE_KEY = process.env.EMISSION_MANAGER_PRIVATE_KEY || '';
@@ -34,7 +35,7 @@ task(
   if (!isAddress(rewardToken)) {
     throw Error('Missing or incorrect rewardToken param');
   }
-  const provider = new JsonRpcProvider('https://rpc.astar.network:8545');
+  const provider = new JsonRpcProvider(getParamPerNetwork(NETWORKS_RPC_URL, networkName));
 
   const emissionManager = new Wallet(EMISSION_MANAGER_PRIVATE_KEY, provider);
   const admin = new Wallet(INCENTIVES_CONTROLLER_ADMIN_PRIVATE_KEY, provider);
